1b1725c5581bbef8683033693e1f08457c1add4b
lidiamoskovenko <130401685+lidiamoskovenko@users.noreply.github.com>
2023-12-10 18:49:17 +0100

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,47 +1,54 @@
-
-import React, { Component } from "react";
+import React, { useState } from "react";
 import { Section } from "./Section";
-import FeedbackOptions  from "./FeedbackOptions";
+import FeedbackOptions from "./FeedbackOptions";
 import Statistics from "./Statistics";
 
-class App extends Component {
-  state = {
+const App = () => {
+  const [feedback, setFeedback] = useState({
     good: 0,
     neutral: 0,
     bad: 0,
-  };
+  });
 
-  makeFeedback = (option) => {
-    this.setState((prevState) => ({
-      [option]: prevState[option] + 1,
+  const makeFeedback = (option) => {
+    setFeedback((prevFeedback) => ({
+      ...prevFeedback,
+      [option]: prevFeedback[option] + 1,
     }));
   };
 
-  render() {
-    const { good, neutral, bad } = this.state;
-    const total = good + neutral + bad;
-    const percentage = Math.round((good / total) * 100);
+  const { good, neutral, bad } = feedback;
+  const total = good + neutral + bad;
+  const percentage = Math.round((good / total) * 100);
 
-    return (
-      <div>
-        <Section title="Please leave feedback">
-          <>
-            <FeedbackOptions options={{ good, neutral, bad }} onLeaveFeedback={this.makeFeedback} />
-            {total > 0 ? (
-              <Statistics good={good} neutral={neutral} bad={bad} total={total} percentage={percentage} />
-            ) : (
-              <div style={{
+  return (
+    <div>
+      <Section title="Please leave feedback">
+        <>
+          <FeedbackOptions options={feedback} onLeaveFeedback={makeFeedback} />
+          {total > 0 ? (
+            <Statistics
+              good={good}
+              neutral={neutral}
+              bad={bad}
+              total={total}
+              percentage={percentage}
+            />
+          ) : (
+            <div
+              style={{
                 fontSize: "24px",
                 fontWeight: "bold",
                 marginTop: "25px",
-        
-              }}>"There is no feedback else"</div>
-            )}
-          </>
-        </Section>
-      </div>
-    );
-  }
-}
+              }}
+            >
+              "There is no feedback else"
+            </div>
+          )}
+        </>
+      </Section>
+    </div>
+  );
+};
 
 export default App;
